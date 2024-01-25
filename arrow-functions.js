aa9d6e8612d87464d8f86d02468d716deb706afc
Ben Fashan <benfashan71@msn.com>
2024-01-25 15:54:48 +0000

    updated arrow functions challenge

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,39 @@
  */
 
 // Vanilla JavaScript Function
+// function addTwoNumbers(a, b) {
+//     //code block
+//     return a + b;
+// }
+// let sum = addTwoNumbers(3, 5);
+// console.log(sum); 
 
 // Arrow Function With Parameters
+const addTwoNumbers = (a, b) => {
+    return a + b;
+}
+let sum = addTwoNumbers(3, 5);
+console.log(sum);
 
 // Single Line Arrow Function With Parameters
+const addTwoNumbers2 = (a, b) => a + b;
+let sum2 = addTwoNumbers2(5, 5);
+console.log(sum2);
 
 // Implicit Returns
 
+const saySomething = message => console.log(message);
+saySomething("hello world");
+
+const sayHello = ()  => console.log("hello")
+sayHello();
+
 // Returning Multiple Lines
+
+const returnMultiLine = () => (
+    `<p>
+    this is multi line string set and a p element
+    </p>`
+)
+console.log(returnMultiLine());
+
